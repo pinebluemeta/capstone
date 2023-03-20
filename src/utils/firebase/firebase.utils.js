d0306c8ddbda88e2632b05a414274e51441703f2
metapineblue <unimetapineblue@hotmail.com>
2023-03-20 21:36:59 +0800

    initial commit

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,128 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth';
+
+import { 
+    getFirestore, 
+    doc, 
+    getDoc,
+    getDocs, 
+    setDoc,
+    collection,
+    writeBatch, 
+    query,
+} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCg_skJed-WsXTfGIVDd0A759ICpvSf8yc",
+  authDomain: "crwn-shop-db-3635c.firebaseapp.com",
+  projectId: "crwn-shop-db-3635c",
+  storageBucket: "crwn-shop-db-3635c.appspot.com",
+  messagingSenderId: "121777501515",
+  appId: "1:121777501515:web:88f287b5a49ea3e57be4ce"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: 'select_account',
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    console.log(userAuth);
+};
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => 
+    signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+    signInWithRedirect(auth, googleProvider);
+    
+export const db = getFirestore();
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+  ) => {
+    const batch = writeBatch(db);
+    const collectionRef = collection(db, collectionKey);
+    
+    objectsToAdd.forEach((object) => {
+       const docRef = doc(collectionRef, object.title.toLowerCase());
+       batch.set(docRef, object);
+    });
+  
+    await batch.commit();
+    console.log('done');
+  };
+  
+export const getCategoriesAndDocuments = async () => {
+const collectionRef = collection(db, 'categories');
+const q = query(collectionRef);
+
+const querySnapshot = await getDocs(q);
+const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+}, {});
+
+return categoryMap;
+};
+
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation,
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+  
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+  
+    return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
